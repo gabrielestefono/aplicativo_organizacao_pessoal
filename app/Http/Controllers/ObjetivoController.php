6c96600c34abf370d3ec70d826bb492dc154f974
gabrielestefono <gabrielestefono@hotmail.com>
2024-02-20 09:52:23 -0300

    Iniciando Projeto

---
+++ app/Http/Controllers/ObjetivoController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Objetivo;
+use Illuminate\Http\Request;
+
+class ObjetivoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Objetivo $objetivo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Objetivo $objetivo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Objetivo $objetivo)
+    {
+        //
+    }
+}
